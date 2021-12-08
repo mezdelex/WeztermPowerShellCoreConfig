57ec1dc0b7943b77e5a70bc32f997bbbdbf67f67
Mezdelex <alexcondegomez@gmail.com>
2021-12-09 00:11:46 +0100

    C:\Users\Mezdelex\Documents\PowerShell\Microsoft.PowerShell_profile.ps1 config added

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,20 @@
+# Oh My Posh
+Set-PoshPrompt -Theme jblab_2021
+
+# Icons
+Import-Module -Name Terminal-Icons
+
+# PSReadLine
+Set-PSReadLineOption -EditMode Emacs
+Set-PSReadLineOption -BellStyle None
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineKeyHandler -Chord 'Ctrl+d' -Function DeleteChar
+
+# PSFzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+# Utilities
+function which($command) {
+    Get-Command -Name $command -ErrorAction SilentlyContinue | Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
+}
