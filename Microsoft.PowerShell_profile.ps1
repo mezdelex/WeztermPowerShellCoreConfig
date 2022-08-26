02088caa8ad604de721a681c0b80ae9c20e07756
Mezdelex <alexcondegomez@gmail.com>
2022-08-26 09:47:15 +0200

    Alphabetical rearrange

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,15 @@
+# Icons
+Import-Module -Name Terminal-Icons
+
 # OhMyPosh
 oh-my-posh init pwsh -c ~/.mezdelex.omp.json | Invoke-Expression
 
-# Icons
-Import-Module -Name Terminal-Icons
+# PSFzf
+Import-Module PSFzf
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # PSReadLine
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-
-# PSFzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
