8f7237143931d9ca1381e6902bf882272079c770
Mezdelex <alexcondegomez@gmail.com>
2022-08-28 23:15:44 +0200

    Updated profile config

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,16 +1,17 @@
-# Icons
+# Imports
+Import-Module -Name PSFzf
+Import-Module -Name PSReadLine
 Import-Module -Name Terminal-Icons
 
-# OhMyPosh
-oh-my-posh init pwsh -c ~/.mezdelex.omp.json | Invoke-Expression
-
-# PSFzf
-Import-Module PSFzf
-Set-PsFzfOption -PSReadlineChordProvider "ctrl+f"
-Set-PsFzfOption -PSReadlineChordReverseHistory "ctrl+r"
-
-# PSReadLine
+# Options
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
+
+# Prompt
+oh-my-posh init pwsh -c ~/.mezdelex.omp.json | Invoke-Expression
+
+# Shortcuts
+Set-PsFzfOption -PSReadlineChordProvider "ctrl+f"
+Set-PsFzfOption -PSReadlineChordReverseHistory "ctrl+r"
