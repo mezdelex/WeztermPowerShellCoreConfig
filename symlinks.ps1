b89b38b0f4cd8d94d10caf1d00d535fb78c129d5
mezdelex <alexcondegomez@gmail.com>
2026-01-14 10:03:56 +0100

    refactor(profile): use proper relative path for profile.ps1 symlink

--- symlinks.ps1
+++ symlinks.ps1
@@ -2,8 +2,8 @@ $symlinkPaths = @{
     "$HOME/.config/wezterm/wezterm.lua" = "wezterm.lua";
     "$HOME/.gitconfig" = ".gitconfig";
     "$HOME/AppData/Roaming/yazi/config/yazi.toml" = "yazi.toml";
+    "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" = "profile.ps1";
     "$HOME/prompt.toml" = "prompt.toml";
-    $PROFILE = "Microsoft.PowerShell_profile.ps1";
 }
 
 foreach ($targetPath in $symlinkPaths.Keys)
