d76ddef31c51f9252cf50aad276f82cccb3e768f
mezdelex <alexcondegomez@gmail.com>
2026-02-01 11:46:51 +0100

    feat(flow-launcher): add flow-launcher theme as well; modify symlinks script to create missing paths

--- symlinks.ps1
+++ symlinks.ps1
@@ -1,18 +1,20 @@
-$terminalIconsCurrentDataPath = (Get-ChildItem "$HOME/Documents/PowerShell/Modules/Terminal-Icons/*/Data" -Directory | Sort-Object Parent -Descending | Select-Object -First 1).FullName
+$terminalIconsCurrentDataPath = Get-ChildItem "$HOME/Documents/PowerShell/Modules/Terminal-Icons/*/Data" -Directory | Sort-Object Parent -Descending | Select-Object -First 1 -ExpandProperty FullName
 
 $symlinkPaths = @{
-    "$HOME/.config/wezterm/wezterm.lua" = "wezterm.lua";
-    "$HOME/.gitconfig" = ".gitconfig";
-    "$HOME/.vimrc" = ".vimrc";
-    "$HOME/AppData/Roaming/yazi/config" = "yazi/config";
-    "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" = "profile.ps1";
-    "$HOME/prompt.toml" = "prompt.toml";
-    "$HOME/scoop/persist/k9s/config/config.yaml" = "k9s/config.yaml";
-    "$HOME/scoop/persist/k9s/config/skins/everforest.yaml" = "k9s/skins/everforest.yaml";
-    (Join-Path $terminalIconsCurrentDataPath "/colorThemes/everforest.psd1") = "terminal-icons/colorThemes/everforest.psd1";
-    (Join-Path $terminalIconsCurrentDataPath "/iconThemes/everforest.psd1") = "terminal-icons/iconThemes/everforest.psd1";
+    "$HOME/.config/wezterm/wezterm.lua" = "./wezterm.lua";
+    "$HOME/.gitconfig" = "./.gitconfig";
+    "$HOME/.vimrc" = "./.vimrc";
+    "$HOME/AppData/Roaming/yazi/config" = "./yazi/config";
+    "$HOME/Documents/PowerShell/Microsoft.PowerShell_profile.ps1" = "./profile.ps1";
+    "$HOME/prompt.toml" = "./prompt.toml";
+    "$HOME/scoop/persist/flow-launcher/UserData/Themes" = "./flow-launcher/UserData/Themes"
+    "$HOME/scoop/persist/k9s/config/config.yaml" = "./k9s/config.yaml";
+    "$HOME/scoop/persist/k9s/config/skins/everforest.yaml" = "./k9s/skins/everforest.yaml";
+    (Join-Path $terminalIconsCurrentDataPath "/colorThemes/everforest.psd1") = "./terminal-icons/colorThemes/everforest.psd1";
+    (Join-Path $terminalIconsCurrentDataPath "/iconThemes/everforest.psd1") = "./terminal-icons/iconThemes/everforest.psd1";
 }
 
 $symlinkPaths.GetEnumerator() | ForEach-Object {
-    New-Item -Path $_.Key -ItemType SymbolicLink -Target (Resolve-Path "./$($_.Value)").Path -Force 
+    New-Item -ItemType Directory -Path (Split-Path $_.Key -Parent) -Force | Out-Null
+    New-Item -ItemType SymbolicLink -Path $_.Key -Target (Convert-Path $_.Value) -Force
 }
